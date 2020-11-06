037d0dcf29817e9705ce8505a379451dcb83a410
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-11-06 10:39:49 -0300

    List created and on click events handled

--- app/src/main/java/com/thiagopereira/mylocalmaps/MainActivity.java
+++ app/src/main/java/com/thiagopereira/mylocalmaps/MainActivity.java
@@ -2,13 +2,40 @@ package com.thiagopereira.mylocalmaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		final String menu[] = new String[]{"Minha casa na cidade natal", "Minha casa em Viçosa", "Meu departamento", "Fechar aplicação"};
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+		setListAdapter(arrayAdapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		final String aux = l.getItemAtPosition(position).toString();
+
+		switch(aux) {
+			case "Minha casa na cidade natal":
+				Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+				break;
+			case "Minha casa em Viçosa":
+				Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+				break;
+			case "Meu departamento":
+				Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+				break;
+			case "Fechar aplicação":
+				finish();
+		}
 	}
 }
