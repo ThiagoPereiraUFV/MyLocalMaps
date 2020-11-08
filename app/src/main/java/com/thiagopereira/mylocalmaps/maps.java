9fdbcb950fa394d0f76c95e235768ff629000982
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2020-11-08 11:00:12 -0300

    Starting to get user localization

--- app/src/main/java/com/thiagopereira/mylocalmaps/maps.java
+++ app/src/main/java/com/thiagopereira/mylocalmaps/maps.java
@@ -1,10 +1,17 @@
 package com.thiagopereira.mylocalmaps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,6 +29,11 @@ public class maps extends FragmentActivity implements OnMapReadyCallback {
 	private final LatLng NATAL = new LatLng(-21.12881, -42.374247);
 	private final LatLng DPTO = new LatLng(-21.109725, -42.381738);
 	private GoogleMap map;
+	public LocationManager lm;
+	public Criteria criteria;
+	public String provider;
+	public int TEMPO_REQUISICAO_LATLONG = 5000;
+	public int DISTANCIA_MIN_METROS = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,48 @@ public class maps extends FragmentActivity implements OnMapReadyCallback {
 		setContentView(R.layout.activity_maps);
 
 		((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
+		//Location Manager
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		criteria = new Criteria();
+
+		//Testa se o aparelho tem GPS
+		PackageManager packageManager = getPackageManager();
+		boolean hasGPS = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+
+		//Estabelece critério de precisão
+		if(hasGPS) {
+			criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		} else {
+			criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		//Obtem melhor provedor habilitado com o critério estabelecido
+		provider = lm.getBestProvider(criteria, true);
+
+		if(provider == null) {
+			Log.e("PROVEDOR", "Nenhum provedor encontrado!");
+		} else {
+			Log.i("PROVEDOR", "Está sendo utilizado o provedor: " + provider);
+
+			//Obtem atualizações de posição
+			if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				// TODO: Consider calling
+				//    ActivityCompat#requestPermissions
+				// here to request the missing permissions, and then overriding
+				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+				//                                          int[] grantResults)
+				// to handle the case where the user grants the permission. See the documentation
+				// for ActivityCompat#requestPermissions for more details.
+				return;
+			}
+			lm.requestLocationUpdates(provider, TEMPO_REQUISICAO_LATLONG, DISTANCIA_MIN_METROS, (LocationListener) this);
+		}
 	}
 
 	@Override
